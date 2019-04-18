a318bbc58b40309d925a7af8f8bf6e32a1bb931f
HaykTheKhulyan <hkhulyan@gmail.com>
2019-04-17 23:55:34 -0700

    Drawing and updating star and planet

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
+
 from Resources.settings import *
+from StarClass.Star import Star
+from PlanetClass.Planet import Planet
 
 if __name__ == "__main__":
     pygame.init()
 
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    screen.fill(pygame.Color('white'))
 
     running = True
-    
+
+
+
+    sun = Star(100, 20)
+    earth = Planet(10, 5)
+
+
+
     clock = pygame.time.Clock()
 
     while (running):
@@ -17,6 +28,13 @@ if __name__ == "__main__":
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
+
+        screen.fill(pygame.Color('white'))
+
+        earth.Update(sun)
+        
+        sun.Draw(screen)
+        earth.Draw(screen)
                 
         pygame.display.flip()
         clock.tick(60)
